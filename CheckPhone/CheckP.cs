0d59f482747b920ea5e85d851074fb6799cce2bf
Ahmed <ahmed.ismail14141414@gmail.com>
2024-01-21 03:24:37 +0200

    Add project files.

---
+++ CheckPhone/CheckP.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CheckPhone
+{
+    internal class CheckP
+    {
+        public bool check(string number)
+        {
+            Regex re = new Regex("\\d");
+            if (re.IsMatch(number))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public bool checkL(string number)
+        {
+            if (number.Length == 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CheckPhone/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CheckPhone;
+CheckP phone = new CheckP();
+Console.WriteLine("Enter phone number");
+string input = Console.ReadLine();
+try
+{
+    bool res = phone.checkL(input);
+    bool result = phone.check(input);
+    if (res==true)
+    {
+        if (result == true)
+        {
+            Console.WriteLine("coreect number");
+        }
+        else 
+        {
+            Console.WriteLine("incorrect number");
+        }
+    }
+    else if(res==false)
+    {
+        Console.WriteLine("incorrect number");
+    }
+    else
+    {
+        Console.WriteLine("incorrect number");
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
